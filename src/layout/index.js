cc589910312c09c039ad93cf2248581fe169d159
peymanath <naderidefault@gmail.com>
2023-04-19 22:24:04 +0430

    add layout

---
+++ src/layout/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Head></Head>
+
+			<Header />
+
+			<main id="main">{children}</main>
+
+			<Footer />
+		</>
+	);
+};
+
+export default Layout;
