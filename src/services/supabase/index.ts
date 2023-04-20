1c66ce8d58769c278af6d5182f974ae87c833a6d
peymanath <naderidefault@gmail.com>
2023-04-20 15:07:39 +0430

    update structur

--- src/services/supabase/index.ts
+++ src/services/supabase/index.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/services/types";
+import { Database } from "@/types";
 export const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_URL,
 	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
