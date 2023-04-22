0ae6fb9ce32dd57e2bb2b3befeb735a1fe6a95a3
peymanath <naderidefault@gmail.com>
2023-04-22 23:21:15 +0430

    added typescript to project

--- src/services/supabase/index.ts
+++ src/services/supabase/index.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/types";
 export const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_URL,
 	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
